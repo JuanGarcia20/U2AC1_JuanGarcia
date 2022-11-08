cf7d0eb6c6ed891577fe938ecad3683dbb1bb27b
JuanGarcia20 <115969644+JuanGarcia20@users.noreply.github.com>
2022-11-08 11:07:14 -0600

    Add files via upload

---
+++ U2AC1_JuanGarcia/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2AC1_JuanGarcia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion variables
+            string nombreAlumno, Materia;
+            byte calificacion = 0, matricula;
+
+            //clasificacion de las variables
+            Console.WriteLine("por favor escriba su nombre completo");
+            nombreAlumno = Console.ReadLine();
+            
+            Console.WriteLine("por favor escriba su matricula");
+            matricula = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("por favor escriba la materia que desea ver la calificacion");
+            Materia = Console.ReadLine();
+
+
+            //if
+            if (calificacion >= 70)
+            {
+                Console.WriteLine("si acreditaste la materia ");
+
+            }
+            else
+            {
+                Console.WriteLine("no acreditaste el curso deberas de acreditar la materia nuevamente");
+            }
+        } 
+    }
+}
